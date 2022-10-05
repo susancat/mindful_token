d22493cc4ed5ce5a8da7b209fd804bcf0b343fdc
susancat <zhangjiayu71@gmail.com>
2022-10-05 11:33:34 +0800

    add lazyloading for homepage

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,61 @@
+import dynamic from 'next/dynamic';
+import { Suspense } from 'react';
+
 import homeStyles from '../styles/Home.module.css';
 import Layout from '../components/IndexPageLayout';
 import Head from '../components/homepage/Head';
-import Intro from '../components/homepage/Intro';
-import Gallery from '../components/homepage/Gallery';
-import Create from '../components/homepage/Create';
-import Donate from '../components/homepage/Donate';
-import Draw from '../components/homepage/Draw';
-import Redeem from '../components/homepage/Redeem';
-import Team from '../components/homepage/Team';
-import Mintpass from '../components/homepage/Mintpass';
+// import Intro from '../components/homepage/Intro';
+// import Gallery from '../components/homepage/Gallery';
+// import Donate from '../components/homepage/Donate';
+// import Draw from '../components/homepage/Draw';
+// import Redeem from '../components/homepage/Redeem';
+// import Team from '../components/homepage/Team';
+// import Mintpass from '../components/homepage/Mintpass';
 
+const Intro = dynamic(() => import('../components/homepage/Intro'), {
+  suspense: false,
+})
+
+const Gallery = dynamic(() => import('../components/homepage/Gallery'), {
+  suspense: false,
+})
+
+const Donate = dynamic(() => import('../components/homepage/Donate'), {
+  suspense: false,
+})
+
+const Draw = dynamic(() => import('../components/homepage/Draw'), {
+  suspense: false,
+})
+
+const Redeem = dynamic(() => import('../components/homepage/Redeem'), {
+  suspense: false,
+})
+
+const Team = dynamic(() => import('../components/homepage/Team'), {
+  suspense: false,
+})
+
+const Mintpass = dynamic(() => import('../components/homepage/Mintpass'), {
+  suspense: false,
+})
 export default function Home() {
   return(
     <div className={homeStyles.main}>
       <Head />
-      <Intro />
-      <Gallery />
-      <Draw />
-      <Mintpass />
-      <Donate />
-      <Redeem />
-      <Team />
+      <Suspense fallback={
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      }>
+        <Intro />
+        <Gallery />
+        <Draw />
+        <Mintpass />
+        <Donate />
+        <Redeem />
+        <Team />
+      </Suspense>
     </div>
   )
 }
