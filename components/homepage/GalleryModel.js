37919358cb094eb97f10b78fc4faebb40a4a784f
susancat <zhangjiayu71@gmail.com>
2022-10-05 13:34:09 +0800

    fix 3D model load

--- components/homepage/GalleryModel.js
+++ components/homepage/GalleryModel.js
@@ -9,42 +9,42 @@ const GalleryModel = (props) => {
     const [modalShow, setModalShow] = useState(false);
     const [loaded, setLoaded] = useState(false);
     const [nft,setNft] = useState();
-
+    const { index, metadata, image } = props
     useEffect(() => {
         loadNFT();
-    },[]);
+        const modelViewer = document.getElementById(`model${index}`);
+        if(modelViewer) {
+            modelViewer.addEventListener("load", () => {
+                let material = modelViewer.model.materials[0];
+                let newTexture = async (imageSource) => {
+                    return await modelViewer.createTexture(imageSource)
+                }
+                let initTexture = async (attr, channel, imageSource) => {
+                    const texture = await newTexture(imageSource);
+                    material[attr][channel].setTexture(texture);
+                }
+                let source = image;
+                initTexture('pbrMetallicRoughness', 'baseColorTexture', source)            
+            }
+        )};
+    });
 
     async function loadNFT() {
-        const metadata = await axios.get(`${props.metadata}`);
+        const mtdata = await axios.get(`${metadata}`);
         let nft = {
-            texture:metadata.data.image,
-            name:metadata.data.name,
-            author:metadata.data.author,
-            description:metadata.data.description
+            texture:mtdata.data.image,
+            name:mtdata.data.name,
+            author:mtdata.data.author,
+            description:mtdata.data.description
         }
         setNft(nft);
         setLoaded(true); 
-        load3DModel();
         //the elements will be created after render, so the above shouldn't be called in rendering
     }
 
     async function load3DModel() {
-      const modelViewer = await document.getElementById(`model${props.index}`);
-      modelViewer.addEventListener("load", () => {
-      
-        let material = modelViewer.model.materials[0];
-        let newTexture = async (imageSource) => {
-          return await modelViewer.createTexture(imageSource)
-        }
-        let initTexture = async (attr, channel, imageSource) => {
-          const texture = await newTexture(imageSource);
-          material[attr][channel].setTexture(texture);
-        }
-        let source = props.image;
-        initTexture('pbrMetallicRoughness', 'baseColorTexture', source)            
-    });
-  
-    }
+        
+        }   
     if (loaded === false && !nft) return (
         <h1 className="px-20 py-10 text-3xl">
             <div className="loadingAnim"></div>
@@ -55,7 +55,7 @@ const GalleryModel = (props) => {
         <Card className={styles.card} onClick={() => setModalShow(true)}>
             {/* src is the 3D model, poster is the texture */}
             <model-viewer 
-                id={`model${props.index}`}
+                id={`model${index}`}
                 src="https://cdn.glitch.me/fba9d1c3-4e26-47cd-867f-19660ae37f80%2Ftest-whale-scale-root-bone.glb?v=1636271548450"
                 ios-src="https://cdn.glitch.me/fba9d1c3-4e26-47cd-867f-19660ae37f80%2Ftest-whale-scale-root-bone-tex.usdz?v=1636599386704"
                 poster="https://cdn.glitch.me/fba9d1c3-4e26-47cd-867f-19660ae37f80%2Funnamed.png?v=1636256018924"
